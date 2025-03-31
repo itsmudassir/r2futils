c010d44b2eb83379aa49637e6b26ea34adb3a61d
Muhammad Mudassir <m.mudassir2015@gmail.com>
2025-03-31 15:34:21 +0500

    v1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="r2futils",
+    version="0.1",
+    packages=find_packages(),
+    description="A simple package to print version info",
+    author="Your Name",
+    author_email="your.email@example.com",
+    zip_safe=False,
+)
